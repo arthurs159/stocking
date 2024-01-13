c9e27638951d22f619df92f6ad362b3300239b7f
Arthur Scarpati <arthur_159@hotmail.com>
2024-01-13 14:53:46 -0300

    Configurando h2 no projeto

---
+++ src/main/java/br/com/stocking/product/ProductRepository.java
@@ -0,0 +1,7 @@
+package br.com.stocking.product;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository {
+}
---
+++ src/main/java/br/com/stocking/product/ProductService.java
@@ -0,0 +1,9 @@
+package br.com.stocking.product;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+}
